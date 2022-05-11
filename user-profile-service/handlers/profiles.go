e3044a8c43fac36275c029578363255b4974bb1c
RyanLiu2015 <ryanliu2015@outlook.com>
2022-05-11 14:37:38 -0700

    fix cors

--- user-profile-service/handlers/profiles.go
+++ user-profile-service/handlers/profiles.go
@@ -53,6 +53,7 @@ func (p *ProfileHandler) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 	rw.Header().Set("Access-Control-Allow-Origin", "*")
 	rw.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 	rw.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	rw.WriteHeader(http.StatusOK)
 
 	if req.Method == http.MethodPost {
 		fmt.Println("post received lmao")
