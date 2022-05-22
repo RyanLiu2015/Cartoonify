e44e5eef3a5a3fbf17bc4ebf637b96ffe76066eb
RyanLiu2015 <ryanliu2015@outlook.com>
2022-05-22 14:39:59 -0700

    finish post new feed function

--- user-profile-service/data-access/feeds.go
+++ user-profile-service/data-access/feeds.go
@@ -1,35 +1,20 @@
 package data_access
 
-type Feeds struct {
-	author_id           int
-	resource_identifier string
-	upvote_count        int
-	share_count         int
+type Feed struct {
+	AuthorId           int
+	ResourceIdentifier string
+	UpvoteCount        int
+	CommentCount       int
+	ShareCount         int
+	CreatedAt          string
 }
 
-func (obj *UserDataAccessObject) InsertNewFeed(feeds Feeds) {
-	obj.db.Select("author_id", "resource_identifier", "upvote_count", "share_count").Create(&feeds)
+func (obj *UserDataAccessObject) InsertNewFeed(feed Feed) {
+	obj.db.Select("author_id", "resource_identifier").Create(&feed)
 }
 
-func (obj *UserDataAccessObject) DisplayUserFeeds(feeds []Feeds) {
-	var page int = 2
-	var pageSize int = 5
-	var total int = 0
-	obj.db.Model(&Feeds{}).Count(&total)
-
-	offset := (page - 1) * pageSize
-	obj.db.Order("fid DESC").Offset(offset).Limit(pageSize).Find(&feeds)
-
-	// c.JSON(http.StatusOK, gin.H{
-	// 	"code":    200,
-	// 	"message": "success",
-	// 	"data"   : map[string]interface{}{
-	// 		"data" : data,
-	// 		"total": total,
-	// 		"page" : page,
-	// 		"pageSize": pageSize,
-	// 	},
-	// })
-	// return
-
+func (obj *UserDataAccessObject) DisplayUserFeeds(page int) []Feed {
+	var ret []Feed
+	obj.db.Raw("SELECT * from user order by created_at desc limit ?, 10", page*10).Scan(&ret)
+	return ret
 }
